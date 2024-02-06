7c39f18360d7412486907aa23a5fa1e7c1f56834
Peter Cai <xicai@blackberry.com>
2024-02-06 15:30:35 -0500

    qnx: Use --define-prefix for pkg-config-wrapper
    pkg-config documentation indicates that `--define-prefix` will override other variables that begin with the default `prefix` variable as well, which is exactly what we want, since not all pkg-configs are generated with the `prefix` variable in mind.
    
    For example, consider the following pkg-config snippet:
    
    ```
    prefix=/usr/local
    libdir=/usr/local/lib
    includedir=/usr/local/include
    ```
    
    Using `--define-variable=prefix=/staging/prefix/on/host` would not change the value of `libdir` and `includedir`, while simply specifying `--define-prefix` will have them figured out automatically.

--- qnx/build/pkg-config-wrapper.sh
+++ qnx/build/pkg-config-wrapper.sh
@@ -4,7 +4,7 @@ export PKG_CONFIG_PATH=
 export PKG_CONFIG_LIBDIR=${INSTALL_ROOT_WITH_PREFIX}/lib/pkgconfig
 
 PKG_CONFIG_FLAGS=(
-  --define-variable=prefix=${INSTALL_ROOT_WITH_PREFIX}
+  --define-prefix
   # Used by .pc files that wrap libraries shipped with QNX
   --define-variable=qnx_target=${QNX_TARGET}
   --define-variable=cpu_dir=${CPUVARDIR}
