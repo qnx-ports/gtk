d34c7cf84d49120dd2b3005bf9aa319a18101c13
Peter Cai <xicai@blackberry.com>
2024-02-21 10:02:44 -0500

    gdk: qnxscreen: Update copyright years to 2024
    since this is technically a new port.

--- gdk/qnxscreen/gdkdisplay-qnxscreen-data.h
+++ gdk/qnxscreen/gdkdisplay-qnxscreen-data.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 BlackBerry Limited.
+ * Copyright (C) 2024 BlackBerry Limited.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
