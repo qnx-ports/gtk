eb7319c2ce5242be8f35b3c09d2b3f750d6fc586
Shreya Arun Naik <sarunnaik@qnx.com>
2025-04-01 19:55:47 +0000

    gtk: Update default image-lookup path for qnx_examples

--- qnx_examples/qnx_examples_common.mk
+++ qnx_examples/qnx_examples_common.mk
@@ -33,7 +33,7 @@ EXTRA_LIBVPATH += $(GTK4_INSTALL_ROOT)/lib
 LDFLAGS += -N2048K
 
 # Default image location on the target is at /base/gtk4/etc/images
-CCFLAGS+=-DIMAGE_RESOURCE_DIR=\"/system/local/etc/gtk4/images\"
+CCFLAGS+=-DIMAGE_RESOURCE_DIR=\"/data/home/qnxuser/share/images\"
 
 # Uncomment this line, or add IOSOCK_BUILD=1 to "make install" command line, if linking against iosock GTK4 libraries
 # i.e. IOSOCK_BUILD=1 make clean install
