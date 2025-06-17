21c153657d6c27407fe42307bc85a3af749b4fb6
Neil Adams <nadams@blackberry.com>
2025-06-17 12:45:39 -0400

    Code refactoring

--- qnx_examples/gtk4_patientmonitor/src/gtk4_patientmonitor.c
+++ qnx_examples/gtk4_patientmonitor/src/gtk4_patientmonitor.c
@@ -63,101 +63,25 @@ double pulse_pattern_ekg[25] = {1.75, 1.75, 1.75, 1.25, 0.25, 0.25, 0.25, 1.5, 1
 double pulse_pattern_pleth[25] = {-5.0, -6.0, -7.0, -8.0, -9.0, -9.25, -9.0, -5.0, -4.0, -2.0, -1.0, 0.0, 0.25, 0.0, -0.25, -0.5, -1.0, -2.0, -3.0, -3.5, -4.0, -4.5, -5.0, -5.0, -5.0};
 double pulse_pattern_resp[25] = {6.0, 6.0, 6.0, 5.5, 5.0, 4.75, 4.0, 3.5, 3.0, 2.75, 2, 1.5, 1.25, 1.0, 1.0, 1.0, 1.25, 1.5, 2.0, 2.75, 3.0, 3.5, 4.0, 5.0, 5.75};
  
+
 static gboolean update_animation(gpointer user_data) {
     GtkWidget *drawing_area = user_data;
- 
-    static int counter = 0;
-    if (counter%40 == 0) { //40 * 25 milliseconds (from timer delay) = 1000 milliseconds = 1 second
-        int pulse_change = rand() % (2*max_pulse_change + 1) - max_pulse_change;
-        pulse_value += pulse_change;
-        if(pulse_value > 120){
-            pulse_value = 120;
-        }
-        else if(pulse_value < 50){
-            pulse_value = 50;
-        }
+
+    if (g_random_int_range(0, 40) == 0) {
+        pulse_value = CLAMP(pulse_value + g_random_int_range(-max_pulse_change, max_pulse_change + 1), 50, 120);
+        respiratory_rate_value = CLAMP(respiratory_rate_value + g_random_int_range(-max_respiratory_change, max_respiratory_change + 1), 19, 23);
+        oximeter_percentage = CLAMP(oximeter_percentage + g_random_int_range(-max_oximeter_change, max_oximeter_change + 1), 91, 100);
+        systolic_bp = CLAMP(systolic_bp + g_random_int_range(-max_bp_change, max_bp_change + 1), 120, 140);
+        diastolic_bp = CLAMP(diastolic_bp + g_random_int_range(-max_bp_change, max_bp_change + 1), 80, 90);
+        body_temperature += ((double)rand() / RAND_MAX * 2 * max_temp_change) - max_temp_change;
+        body_temperature = CLAMP(body_temperature, 97.0, 99.9);
+        co2_level = CLAMP(co2_level + g_random_int_range(-max_co2_change, max_co2_change + 1), 35, 45);
     }
 
-    counter++;
-
-    static int counter2 = 0;
-    if (counter2 % 40 == 0) { 
-        int respiratory_change = rand() % (2 * max_respiratory_change + 1) - max_respiratory_change;
-        respiratory_rate_value += respiratory_change;
-        if (respiratory_rate_value > 23) {
-            respiratory_rate_value = 23; 
-        } else if (respiratory_rate_value < 19) {
-            respiratory_rate_value = 19; 
-        }
-    }
-
-    counter2++;
- 
-    static int counter3 = 0;
-    if (counter3 % 40 == 0) { 
-        int oximeter_change = rand() % (2 * max_oximeter_change + 1) - max_oximeter_change;
-        oximeter_percentage += oximeter_change;
-        if (oximeter_percentage > 100) {
-            oximeter_percentage = 100; 
-        } else if (oximeter_percentage < 91) {
-            oximeter_percentage = 91;  
-        }
-    }
-
-    counter3++;
-
-    static int counter4 = 0;
-    if (counter4 % 40 == 0) { 
-        int systolic_change = rand() % (2 * max_bp_change + 1) - max_bp_change;
-        int diastolic_change = rand() % (2 * max_bp_change + 1) - max_bp_change;
- 
-        systolic_bp += systolic_change;
-        if (systolic_bp > 140) {
-            systolic_bp = 140; 
-        } else if (systolic_bp < 120) {
-            systolic_bp = 120; 
-        }
- 
-        diastolic_bp += diastolic_change;
-        if (diastolic_bp > 90) {
-            diastolic_bp = 90; 
-        } else if (diastolic_bp < 80) {
-            diastolic_bp = 80;  
-        }
-    }
-
-    counter4++;
- 
-    static int counter5 = 0;
-    if (counter5 % 40 == 0) { 
-        float temp_change = ((float)rand() / RAND_MAX) * 2 * max_temp_change - max_temp_change;
-        body_temperature += temp_change;
-        if (body_temperature > 99.9) {
-            body_temperature = 99.9; 
-        } else if (body_temperature < 97) {
-            body_temperature = 97;  
-        }
-    }
-
-    counter5++;
- 
-    static int counter6 = 0;
-    if (counter6 % 40 == 0) { 
-        int co2_change = rand() % (2 * max_co2_change + 1) - max_co2_change;
-        co2_level += co2_change;
-        if (co2_level > 45) {
-            co2_level = 45; 
-        } else if (co2_level < 35) {
-            co2_level = 35;  
-        }
-    }
-
-    counter6++;
- 
     gtk_widget_queue_draw(drawing_area);
     return G_SOURCE_CONTINUE;
 }
- 
+
 void update_wave_position(double *current_x, double speed) {
     *current_x -= speed;
     if (*current_x <= 0) {
@@ -183,6 +107,13 @@ void draw_waveform(cairo_t *cr, const double *buffer, double current_x, double y
 
     // Draw the wave to the screen
     cairo_stroke(cr);
+}
+
+void draw_label(cairo_t *cr, const char *text, double x, double y, double r, double g, double b, int font_size) {
+    cairo_set_source_rgb(cr, r, g, b);
+    cairo_move_to(cr, x, y);
+    cairo_set_font_size(cr, font_size);
+    cairo_show_text(cr, text);
 }
 
 static void draw_callback(GtkDrawingArea *drawing_area, cairo_t *cr, int width, int height, gpointer data) {
@@ -212,127 +143,61 @@ static void draw_callback(GtkDrawingArea *drawing_area, cairo_t *cr, int width, 
     cairo_line_to(cr, SCREEN_WIDTH, SCREEN_HEIGHT-100);
     cairo_stroke(cr);
  
+    // Patient name
+    draw_label(cr, "Patient: Mr. Sick", 40, 50, 0, 0, 0, 30);
+
     // Draw pulse value
-    cairo_set_source_rgb(cr, 1, 0, 0);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_2_OFFSET, 235);
-    cairo_set_font_size(cr, 125);
-    char pulse_text[20]; // Buffer to hold text
-    snprintf(pulse_text, sizeof(pulse_text), "%d", pulse_value);
-    cairo_show_text(cr, pulse_text);
-    cairo_set_source_rgb(cr, 1, 1, 0);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_2_OFFSET, 285);
-    cairo_set_font_size(cr, 30);
-    cairo_show_text(cr, "Pulse");
- 
-    // Draw respiratory value
-    cairo_set_source_rgb(cr, 0, 0, 1);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_2_OFFSET, 880);
-    cairo_set_font_size(cr, 125);
-    char respiratory_text[30]; // Buffer to hold text
-    snprintf(respiratory_text, sizeof(respiratory_text), "%d", respiratory_rate_value);
-    cairo_show_text(cr, respiratory_text);
-    cairo_set_source_rgb(cr, 1, 1, 0);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_2_OFFSET, 920);
-    cairo_set_font_size(cr, 30);
-    cairo_show_text(cr, "RESP(bpm)");
+    char text_buf[64];
+    snprintf(text_buf, sizeof(text_buf), "%d", pulse_value);
+    draw_label(cr, text_buf, SCREEN_WIDTH - TEXT_COLUMN_2_OFFSET, 235, 1, 0, 0, 125);
+    draw_label(cr, "Pulse", SCREEN_WIDTH - TEXT_COLUMN_2_OFFSET, 285, 1, 1, 0, 30);
+
+    // Draw temperature value
+    snprintf(text_buf, sizeof(text_buf), "%.1f°", body_temperature);
+    draw_label(cr, text_buf, SCREEN_WIDTH - TEXT_COLUMN_3_OFFSET, 235, 1, 0.5, 0, 125);
+    draw_label(cr, "Temperature", SCREEN_WIDTH - TEXT_COLUMN_3_OFFSET, 285, 1, 1, 0, 30);
+
+    // Draw blood pressure value
+    snprintf(text_buf, sizeof(text_buf), "%d", systolic_bp);
+    draw_label(cr, text_buf, SCREEN_WIDTH - TEXT_COLUMN_2_OFFSET, 540, 0, 1, 0, 125);
+    draw_label(cr, "____", SCREEN_WIDTH - TEXT_COLUMN_2_OFFSET, 540, 0, 1, 0, 125);
+    snprintf(text_buf, sizeof(text_buf), "%d", diastolic_bp);
+    draw_label(cr, text_buf, SCREEN_WIDTH - TEXT_COLUMN_2_OFFSET + 40, 680, 0, 1, 0, 125);
+    draw_label(cr, "Blood Pressure", SCREEN_WIDTH - TEXT_COLUMN_2_OFFSET, 730, 1, 1, 0, 30);
  
     // Draw oximeter value
-    cairo_set_source_rgb(cr, 0, 1, 0.5);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_3_OFFSET, 585);
-    cairo_set_font_size(cr, 125);
-    char oximeter_text[30]; // Buffer to hold text
-    snprintf(oximeter_text, sizeof(oximeter_text), "%d", oximeter_percentage);
-    cairo_show_text(cr, oximeter_text);
-    cairo_set_source_rgb(cr, 1, 1, 0);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_3_OFFSET, 625);
-    cairo_set_font_size(cr, 30);
-    cairo_show_text(cr, "Oximeter");
- 
-    // Draw blood pressure value
-    cairo_set_source_rgb(cr, 0, 1, 0);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_2_OFFSET, 540);
-    cairo_set_font_size(cr, 125);
-    char bp_text[50]; //buffer to hold text
-    snprintf(bp_text, sizeof(bp_text), "%d", systolic_bp);
-    cairo_show_text(cr, bp_text);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_2_OFFSET, 540);
-    snprintf(bp_text, sizeof(bp_text), "____");
-    cairo_show_text(cr, bp_text);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_2_OFFSET+40, 680);
-    snprintf(bp_text, sizeof(bp_text), "%d", diastolic_bp);
-    cairo_show_text(cr, bp_text);
-    cairo_set_source_rgb(cr, 1, 1, 0);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_2_OFFSET, 730);
-    cairo_set_font_size(cr, 30);
-    cairo_show_text(cr, "Blood Pressure");
- 
-    // Draw temperature value
-    cairo_set_source_rgb(cr, 1, 0.5, 0);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_3_OFFSET, 235);
-    cairo_set_font_size(cr, 125);
-    char temp_text[50]; // Buffer to hold text
-    snprintf(temp_text, sizeof(temp_text), "%.1f°", body_temperature);
-    cairo_show_text(cr, temp_text);
-    cairo_set_source_rgb(cr, 1, 1, 0);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_3_OFFSET, 285);
-    cairo_set_font_size(cr, 30);
-    cairo_show_text(cr, "Temperature");
+    snprintf(text_buf, sizeof(text_buf), "%d", oximeter_percentage);
+    draw_label(cr, text_buf, SCREEN_WIDTH - TEXT_COLUMN_3_OFFSET, 585, 0, 1, 0.5, 125);
+    draw_label(cr, "Oximeter", SCREEN_WIDTH - TEXT_COLUMN_3_OFFSET, 625, 1, 1, 0, 30);
 
-    // Patient name 
-    cairo_set_source_rgb(cr, 0, 0, 0); 
-    cairo_move_to(cr, 40, 50); 
-    cairo_set_font_size(cr, 30);
-    cairo_show_text(cr, "Patient: Mr. Sick" );
+    // Draw respiratory value
+    snprintf(text_buf, sizeof(text_buf), "%d", respiratory_rate_value);
+    draw_label(cr, text_buf, SCREEN_WIDTH - TEXT_COLUMN_2_OFFSET, 880, 0, 0, 1, 125);
+    draw_label(cr, "RESP(bpm)", SCREEN_WIDTH - TEXT_COLUMN_2_OFFSET, 920, 1, 1, 0, 30);
 
-    // Draw time
-    time_t current_time;
-    time(&current_time);
- 
-    struct tm *time_info;
-    time_info = localtime(&current_time);
- 
-    char time_string[50];
-    strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", time_info);
- 
-    cairo_set_source_rgb(cr, 1, 1, 1); 
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_2_OFFSET, 1030); 
-    cairo_set_font_size(cr, 30);
-    cairo_show_text(cr, time_string);
- 
     // Draw CO2 level value
-    cairo_set_source_rgb(cr, 0.5, 0, 1);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_3_OFFSET, 880);
-    cairo_set_font_size(cr, 125);
-    char co2_text[50]; // Buffer to hold text
-    snprintf(co2_text, sizeof(co2_text), "%d", co2_level);
-    cairo_show_text(cr, co2_text);
-    cairo_set_source_rgb(cr, 1, 1, 0);
-    cairo_move_to(cr, SCREEN_WIDTH-TEXT_COLUMN_3_OFFSET, 920);
-    cairo_set_font_size(cr, 30);
-    cairo_show_text(cr, "CO2(mmHg)");
+    snprintf(text_buf, sizeof(text_buf), "%d", co2_level);
+    draw_label(cr, text_buf, SCREEN_WIDTH - TEXT_COLUMN_3_OFFSET, 880, 0.5, 0, 1, 125);
+    draw_label(cr, "CO2(mmHg)", SCREEN_WIDTH - TEXT_COLUMN_3_OFFSET, 920, 1, 1, 0, 30);
+    
+    // Draw time
+    time_t now = time(NULL);
+    struct tm *tm_info = localtime(&now);
+    char time_buf[64];
+    strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S", tm_info);
+    draw_label(cr, time_buf, SCREEN_WIDTH - TEXT_COLUMN_2_OFFSET, 1030, 1, 1, 1, 30);
 
-    // Draw first wave (EKG) label
-    cairo_set_source_rgb(cr, 1, 0, 0); // Red
-    cairo_move_to(cr, TEXT_COLUMN_1_OFFSET, 350);
-    cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-    cairo_set_font_size(cr, 30);
-    cairo_show_text(cr, "EKG");
- 
-    // Draw second wave (Pleth) label
-    cairo_set_source_rgb(cr, 0, 1, 0); // Green
-    cairo_move_to(cr, TEXT_COLUMN_1_OFFSET, 705);
-    cairo_show_text(cr, "Pleth");
- 
-    // Draw third wave (RESP) label
-    cairo_set_source_rgb(cr, 0, 0, 1); // Blue
-    cairo_move_to(cr, TEXT_COLUMN_1_OFFSET, 990);
-    cairo_show_text(cr, "RESP");
-        
+    // Draw wave labels
+    draw_label(cr, "EKG", TEXT_COLUMN_1_OFFSET, 350, 1, 0, 0, 30);
+    draw_label(cr, "Pleth", TEXT_COLUMN_1_OFFSET, 705, 0, 1, 0, 30);
+    draw_label(cr, "RESP", TEXT_COLUMN_1_OFFSET, 990, 0, 0, 1, 30);
+
     // Draw waveforms
     draw_waveform(cr,  wave_buffer_ekg,   current_x_ekg,   EKG_Y_OFFSET,   AMPLITUDE, 1.0, 0.0, 0.0);
     draw_waveform(cr2, wave_buffer_pleth, current_x_pleth, PLETH_Y_OFFSET, AMPLITUDE, 0.0, 1.0, 0.0);
     draw_waveform(cr3, wave_buffer_resp,  current_x_resp,  RESP_Y_OFFSET,  AMPLITUDE, 0.0, 0.0, 1.0);
 
+    // Update the wave positions
     update_wave_position(&current_x_ekg, SCROLL_SPEED_EKG);
     update_wave_position(&current_x_pleth, SCROLL_SPEED_PLETH);
     update_wave_position(&current_x_resp, SCROLL_SPEED_RESP);
